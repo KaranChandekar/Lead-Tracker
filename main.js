70a6ce2cec214f2f05b330e6f8006416dbd62474
KaranChandekar <karanchandekar431@gmail.com>
2021-11-22 18:07:42 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let buttonEl = document.getElementById("input-btn")
+let inputEl = document.getElementById("input-el")
+
+buttonEl.addEventListener("click", function () {
+    console.log("button clicked")
+})
