0cce08795b6b2ab0e9726d1ead3a9b8c009014ce
KaranChandekar <karanchandekar431@gmail.com>
2021-11-22 23:18:06 +0530

    added ul tag

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
-    saveEl.textContent = myLeads
 })
 
 for (let i = 0; i < myLeads.length; i++) {
-    console.log(myLeads[i])
-    
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
 }
