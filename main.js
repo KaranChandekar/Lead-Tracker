5a1053426cd6460eab9fd57956acc423ddb11d16
KaranChandekar <karanchandekar431@gmail.com>
2021-11-22 19:09:25 +0530

    added for loop

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-let myLeads = []
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    saveEl.textContent = myLeads
 })
+
+for (let i = 0; i < myLeads.length; i++) {
+    console.log(myLeads[i])
+    
+}
